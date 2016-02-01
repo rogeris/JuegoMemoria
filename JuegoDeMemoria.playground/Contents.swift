1135811d490e1fb138a47997c20f42cca07a6e75
Rogelio Herrerias <roger@Rogelios-MacBook-Pro.local>
2016-02-01 16:21:20 -0700

    adding playgrounds

---
+++ JuegoDeMemoria.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var rango = 0...100
+
+for n in rango{
+    if n % 5 == 0{
+        print("# \(n) Bingo!!!")
+    }
+    if n % 2 == 0{
+        print("# \(n) par!!!")
+    }else{
+        print("# \(n) impar!!!")
+    }
+    if n >= 30 && n <= 40{
+        print("# \(n) Viva Swift!!!")
+    }
+    
+}
