4324e50f216c8891f211bcd1e59b830750f5e195
Rogelio Herrerias <roger@Rogelios-MacBook-Pro.local>
2016-02-20 22:53:42 -0700

    adding velocimetro

---
+++ VelocimetroAutomobil.playground/Contents.swift
@@ -0,0 +1,52 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+enum Velocidades : Int {
+    case Apagado = 0, VelocidadBaja = 20, VelocidadMedia = 50, VelocidadAlta = 120
+    init(velocidadInicial : Velocidades){
+        self = velocidadInicial
+    }
+}
+
+class Auto{
+    var velocidad : Velocidades
+    
+    init(){
+        self.velocidad = Velocidades(velocidadInicial: .Apagado)
+    }
+    
+    func cambioDeVelocidad( ) -> ( actual : Int, velocidadEnCadena: String){
+        let actual : Int = velocidad.rawValue
+        let velocidadEnCadena : String
+        
+        switch actual{
+          case 0 :
+            velocidadEnCadena = "Apagado"
+            velocidad = .VelocidadBaja
+          case 20 :
+            velocidadEnCadena = "Velocidad baja"
+            velocidad = .VelocidadMedia
+          case 50 :
+            velocidadEnCadena = "Velocidad media"
+            velocidad = .VelocidadAlta
+          case 120 :
+            velocidadEnCadena = "Velocidad alta"
+            velocidad = .VelocidadMedia
+          default  : velocidadEnCadena = ""
+        }
+        
+        let resultado = ( actual, velocidadEnCadena )
+        
+        return resultado
+    }
+}
+
+var auto = Auto()
+
+for var i = 0; i < 20 ; i++ {
+    var velocidadActual = auto.cambioDeVelocidad()
+    print("\(velocidadActual.actual),\(velocidadActual.velocidadEnCadena)")
+}
+
+
